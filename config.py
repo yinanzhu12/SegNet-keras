f1afde72ccb80080d8e94e98bc8b5050c9b2dabd
Yinan Zhu <welltempered.ivan@gmail.com>
2018-08-08 13:49:08 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
-training_set_path='SegNet/CamVid/train.txt'
+training_set_path='SegNet/CamVid/train.txt' 
 val_set_path='SegNet/CamVid/val.txt'
 test_set_path='SegNet/CamVid/test.txt'
 model_path='models/'
 
 num_classes=13
 image_shape=(360,480,3)
-num_features=6
 batch_size=10
 epochs=2
 training_set_size=367
